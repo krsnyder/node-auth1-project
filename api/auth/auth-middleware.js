02353d9d8260d88050616ac4cfe1ac090a188c06
krsnyder <kirkrobertsnyder@gmail.com>
2021-04-19 16:09:59 -0400

    finished middleware

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -25,14 +25,6 @@ async function checkUsernameFree(req, res, next) {
   }
 }
 
-/*
-  If the username in req.body does NOT exist in the database
-
-  status 401
-  {
-    "message": "Invalid credentials"
-  }
-*/
 async function checkUsernameExists(req, res, next) {
   const { username } = req.body;
   const users = await Users.find();
@@ -50,19 +42,17 @@ async function checkUsernameExists(req, res, next) {
   }
 }
 
-/*
-  If password is missing from req.body, or if it's 3 chars or shorter
+function checkPasswordLength(req, res, next) {
+  const { password } = req.body;
 
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
+  if (!password || password.length < 3) {
+    res.status(422).json({
+      message: "Password must be longer than 3 chars"
+    })
+  } else {
+    next()
   }
-*/
-function checkPasswordLength(req,res,next) {
-  next()
 }
-
-// Don't forget to add these to the `exports` object so they can be required in other modules
 
 module.exports = {
   restricted,
